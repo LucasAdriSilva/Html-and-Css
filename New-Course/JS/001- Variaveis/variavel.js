b8c83fdd55c55262b4934bfd385f67836f8dafa8
Lucas Adriano Silva <lucasadrisilva@hotmail.com>
2022-01-15 15:50:03 -0300

    feat(JS): Continuação de aprendizado

---
+++ New-Course/JS/001- Variaveis/variavel.js
@@ -0,0 +1,15 @@
+// string 
+var texto = 'Curso de JS'
+
+//number
+var numeroInteiro = -7 //int
+var numeroFracionado = 152.10 //float
+
+//boolean
+var inicio = true
+
+alert(numeroInteiro) //abrir um dialogo
+
+document.write(numeroInteiro) //Mandar para o HTML
+
+console.log(numeroInteiro)
---
+++ New-Course/JS/002- Concatenação/script.js
@@ -0,0 +1,9 @@
+var name = prompt('Digiti o seu nome')
+var idade = prompt('Digiti o seu idade')
+var idade2 = 50
+
+console.log(idade)
+console.log(idade2)
+
+document.write(`<h1>Hello Word, Hi ${name} e sua idade é: ${idade}</h1>`)
+//document.write('<h1>Hello Word, hi ' + name + '</h1>')
