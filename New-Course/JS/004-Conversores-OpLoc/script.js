12792cb40b126bc468343760e0142180aa144450
Lucas Adriano Silva <lucasadrisilva@hotmail.com>
2022-01-20 10:52:03 -0300

    feat(JS): Operadoes Lógicos

---
+++ New-Course/JS/004-Conversores-OpLoc/script.js
@@ -0,0 +1,20 @@
+// -var media = 7
+// var nota = prompt('Digite sua media')
+
+// if(nota >= media){
+//     document.write('Parabens você passou')
+// } else{
+//     document.write('Infelizmente vc reprovou com nota: '+ nota +' não foi o suficiente, a média é 7')
+// }
+
+// var v1 = prompt('Digite 1º numero')
+// var v2 = prompt('Digite 2º numero') //Os dados vem como string
+
+// v1 = parseInt(v1) // Conversor para numerico
+// v2 = parseFloat(v2) // Conversosr de numero quebrados
+// v1 = toString(v1) // Conersor para string podendo usar como metodo '.toString()'
+
+
+// && Porta E if(x=10 && x % 2)
+// || Porta OU if (x=10 || x=11)
+// ! negação  if(x != 10)
