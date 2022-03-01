ccbe5f8c693625e31af0476e14268c6376af72a7
Lucas Adriano Silva <lucasadrisilva@hotmail.com>
2022-02-28 21:54:30 -0300

    feat(APP): Orçamento com Local Storage

---
+++ New-Course/OO/Orçamento Pessoal/app.js
@@ -0,0 +1,58 @@
+class Despesa {
+   constructor(ano, mes, dia, tipo, descricao, valor) {
+      this.ano = ano
+      this.mes = mes
+      this.dia = dia
+      this.tipo = tipo
+      this.descricao = descricao
+      this.valor = valor
+
+   }
+}
+
+class Bd {
+   constructor() {
+      let id = localStorage.getItem('id')
+      
+      if (id === null) {
+         localStorage.setItem('id', 0)
+      }
+   }
+   getProximoId() {
+      let proximoId = localStorage.getItem('id')
+      return parseInt(proximoId) + 1
+   }
+
+   gravar(d) {
+      let id = this.getProximoId()
+
+      localStorage.setItem(id, JSON.stringify(d))
+      localStorage.setItem('id', id)
+   }
+}
+
+
+let bd = new Bd()
+
+
+
+function cadastrarDespesa() {
+   let ano = document.getElementById('ano')
+   let mes = document.getElementById('mes')
+   let dia = document.getElementById('dia')
+   let descricao = document.getElementById('descricao')
+   let valor = document.getElementById('valor')
+   let tipo = document.getElementById('tipo')
+   let despesa = new Despesa(
+      mes.value,
+      ano.value,
+      dia.value,
+      tipo.value,
+      descricao.value,
+      valor.value
+   )
+
+   bd.gravar(despesa)
+
+}
+
