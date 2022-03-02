bd1872e20d1e98be1630bb2160bd71864e947b16
Lucas Adriano Silva <lucasadrisilva@hotmail.com>
2022-03-01 21:17:35 -0300

    feat(APP): Finish aplication

--- New-Course/OO/Orçamento Pessoal/app.js
+++ New-Course/OO/Orçamento Pessoal/app.js
@@ -52,11 +52,42 @@ class Bd {
 			//Verificação de itens removidos
 			if (desp === null) {
 				continue
-			} else {
-				ArrDesp.push(desp)
 			}
+			desp.id = i
+			ArrDesp.push(desp)
+
 		}
 		return ArrDesp
+	}
+
+	pesquisar(despesa) {
+		let despesaFiltrada = Array()
+		despesaFiltrada = this.recuperarTodosRegistros()
+
+		// Filtros
+		if (despesa.ano != '') {
+			despesaFiltrada = despesaFiltrada.filter(d => d.ano == despesa.ano)
+		}
+		if (despesa.mes != '') {
+			despesaFiltrada = despesaFiltrada.filter(d => d.mes == despesa.mes)
+		}
+		if (despesa.dia != '') {
+			despesaFiltrada = despesaFiltrada.filter(d => d.dia == despesa.dia)
+		}
+		if (despesa.tipo != '') {
+			despesaFiltrada = despesaFiltrada.filter(d => d.tipo == despesa.tipo)
+		}
+		if (despesa.descricao != '') {
+			despesaFiltrada = despesaFiltrada.filter(d => d.descricao == despesa.descricao)
+		}
+		if (despesa.valor != '') {
+			despesaFiltrada = despesaFiltrada.filter(d => d.valor == despesa.valor)
+		}
+		return despesaFiltrada
+	}
+
+	remover(id) {
+		localStorage.removeItem(id)
 	}
 }
 
@@ -90,7 +121,7 @@ function cadastrarDespesa() {
 		document.getElementById('modal_btn').innerHTML = 'Voltar'
 
 		$('#modalRegistraDespesa').modal('show')
-		
+
 		//Zerando os campos
 		ano.value = ''
 		mes.value = ''
@@ -112,11 +143,14 @@ function cadastrarDespesa() {
 
 }
 
-function carregaListaDespesas() {
-	let despesas = Array()
-	despesas = bd.recuperarTodosRegistros()
+function carregaListaDespesas(despesas = Array(), filtro = false) {
+	if (despesas.length === 0 && filtro == false) {
+		despesas = bd.recuperarTodosRegistros()
+	}
 	//selecionando elemento Tbody
+
 	let listDesp = document.getElementById('listaDespesas')
+	listDesp.innerHTML = ''
 
 	despesas.forEach(function (d) {
 
@@ -139,8 +173,38 @@ function carregaListaDespesas() {
 				break
 		}
 		linha.insertCell(1).innerHTML = d.tipo
-
 		linha.insertCell(2).innerHTML = d.descricao
 		linha.insertCell(3).innerHTML = d.valor
+
+		//Botão de exclusão
+		let btn = document.createElement("Button")
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class="fas fa-times"></i>'
+		btn.id = `id_despesa${d.id}`
+
+		
+		console.log(btn.id = d.id)
+		btn.onclick = function () {
+			let id = this.id.replace('id_despesa_', '')
+			bd.remover(this.id)
+			window.location.reload()
+		}
+		linha.insertCell(4).append(btn)
+
 	})
+}
+
+function pesquisarDespesa() {
+	let ano = document.getElementById('ano').value
+	let mes = document.getElementById('mes').value
+	let dia = document.getElementById('dia').value
+	let tipo = document.getElementById('tipo').value
+	let descricao = document.getElementById('descricao').value
+	let valor = document.getElementById('valor').value
+
+	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+	let despesas = bd.pesquisar(despesa)
+
+	carregaListaDespesas(despesas, true)
 }
