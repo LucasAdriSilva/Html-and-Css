5c587951be1e1c5603930358bc1b9199b5914a05
Lucas Adriano <lucasadrisilva@hotmail.com>
2022-03-08 07:17:39 -0300

    feat(Project): Usando altura e lagura da Div

--- New-Course/OO/Capturando_dados/Maps.js
+++ New-Course/OO/Capturando_dados/Maps.js
@@ -1,7 +1,7 @@
 
 
 function creatMedieval() {
-    console.log(` A ${ cidadeAleatoria(0,5) } vai ficar na posição ${ posicaoRandom() }`)
+    posicaoRandom()
     
 }
 
@@ -18,7 +18,7 @@ function cidadeAleatoria(min, max) {
     let x = Math.floor(Math.random() * (max - min + 1)) + min;
     switch (x) {
         case 0:
-            return 'cidade1'
+            return 'assets/oculos.png'
         case 1:
             return 'cidade2'
         case 2:
@@ -28,30 +28,34 @@ function cidadeAleatoria(min, max) {
         case 4:
             return 'cidade5'
         case 5:
-            return 'cidade6s'
+            return 'cidade6'
     }
 }
 
 function posicaoRandom() {
-    let altura = window.innerWidth
-    let largura = window.innerHeight
-    let x = Math.floor(Math.random() * largura) - 90
-    let y = Math.floor(Math.random() * altura) - 90
+    let div = document.getElementById('creat_maps')
+    console.log('div.window.innerHeight')
+
+    let altura = document.querySelector('#creat_maps').offsetHeight
+    let largura = document.querySelector('#creat_maps').offsetWidth
+
+    console.log(altura)
+    console.log(largura)
+
+    let x = Math.floor(Math.random() * largura) - 170
+    let y = Math.floor(Math.random() * altura) - 170
     x = x < 0 ? 0 : x
     y = y < 0 ? 0 : y
 
-    let cidade = cidadeAleatoria(0, 5)
+    let cidade = document.createElement('img');
+    cidade.src = cidadeAleatoria(0,0)
+    cidade.style.left = x + 'px';
+    cidade.style.top = y + 'px';
+    cidade.style.position = 'absolute';
+    cidade.id = 'Cidade';
 
-    var mosquito = document.createElement('img')
-    cidade.src = 'assets/pergaminho.png'
-    mosquito.className = tamanhoAleatorio(0, 3) + ' ' + ladoAleatorio(0, 1)
-    mosquito.style.left = posicaoX + 'px'
-    mosquito.style.top = posicaoY + 'px'
-    mosquito.style.position = 'absolute'
-    mosquito.id = 'mosquito'
-    document.body.appendChild(mosquito)
-    mosquito.onclick = function () {
-        this.remove()
-    }
+    div.appendChild(cidade)
+    console.log(cidade)
+
 
 }
